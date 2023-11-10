cf76170d33086ee20a6e001c0462a43f9d9cd8c5
MrSmart00 <nexearth@gmail.com>
2023-11-11 01:18:24 +0900

    Add: Session 1 layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: require_trailing_commas
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,9 +14,76 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: FractionallySizedBox(
+            widthFactor: 0.5,
+            child: Column(children: [
+              Spacer(),
+              WeatherView(),
+              Expanded(child: Column(
+                children: [
+                  Padding(padding: EdgeInsets.only(top: 80)),
+                  ButtonArea()
+                ])
+              ),
+            ])
+          ),
         ),
       ),
+    );
+  }
+}
+
+class WeatherView extends StatelessWidget {
+  const WeatherView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(children: [
+      AspectRatio(
+        aspectRatio: 1, 
+        child: Placeholder()
+      ),
+      Padding(
+        padding: EdgeInsets.symmetric(vertical: 16),
+        child: TempertureView()
+      )
+    ]);
+  }
+}
+
+class TempertureView extends StatelessWidget {
+  const TempertureView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).textTheme.labelLarge;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Text(
+          '** ℃', 
+          style: theme?.copyWith(color: Colors.blue)
+        ),
+        Text(
+          '** ℃', 
+          style: theme?.copyWith(color: Colors.red)
+        ),
+      ]
+    );
+  }
+}
+
+class ButtonArea extends StatelessWidget {
+  const ButtonArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        TextButton(onPressed: () {}, child: const Text('Close')),
+        TextButton(onPressed: () {}, child: const Text('Reload')),
+      ],
     );
   }
 }
